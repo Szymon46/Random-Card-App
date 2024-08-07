58a11f49425f6c844e785b1626ed3e17f7c3c861
Szymon Dembowski <szymondembowski1234@gmail.com>
2024-08-07 16:24:48 +0200

    animation from center is working. Picking a card is to do.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 "use strict";
 const startBtn = document.querySelector(".start-btn");
-const images = document.querySelectorAll(".board__img");
+const images = document.querySelectorAll(".img");
 let listen = true;
 function startApp() {
     let i = 1;
     startBtn.style.animationName = "fade-out";
     images.forEach((item) => {
         item.style.animationDelay = `${i}s`;
-        item.style.animationName = "fade-in";
+        item.style.animationName = `fade-in${i}`;
         item.style.cursor = "pointer";
         i++;
     });
--- ts/script.ts
+++ ts/script.ts
@@ -1,13 +1,12 @@
 const startBtn: HTMLButtonElement = document.querySelector(".start-btn")!;
-const images: NodeListOf<HTMLImageElement> =
-  document.querySelectorAll(".board__img");
+const images: NodeListOf<HTMLImageElement> = document.querySelectorAll(".img");
 let listen: boolean = true;
 function startApp() {
   let i: number = 1;
   startBtn.style.animationName = "fade-out";
   images.forEach((item) => {
     item.style.animationDelay = `${i}s`;
-    item.style.animationName = "fade-in";
+    item.style.animationName = `fade-in${i}`;
     item.style.cursor = "pointer";
     i++;
   });

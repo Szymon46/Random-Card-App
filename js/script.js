aec4207ce0b301d9278912504823e8b40438e0c4
Szymon Dembowski <szymondembowski1234@gmail.com>
2024-08-07 15:41:09 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 "use strict";
 const startBtn = document.querySelector(".start-btn");
 const images = document.querySelectorAll(".board__img");
+let listen = true;
 function startApp() {
     let i = 1;
     startBtn.style.animationName = "fade-out";
     images.forEach((item) => {
         item.style.animationDelay = `${i}s`;
         item.style.animationName = "fade-in";
+        item.style.cursor = "pointer";
         i++;
     });
 }
+function handleCard(item) {
+    if (listen) {
+        item.style.animationDelay = "0s";
+        item.style.animationName = "slide";
+    }
+}
 startBtn.addEventListener("click", startApp);
+images.forEach((item) => {
+    item.addEventListener("click", () => handleCard(item));
+});
--- ts/script.ts
+++ ts/script.ts
@@ -1,15 +1,26 @@
 const startBtn: HTMLButtonElement = document.querySelector(".start-btn")!;
 const images: NodeListOf<HTMLImageElement> =
   document.querySelectorAll(".board__img");
-
+let listen: boolean = true;
 function startApp() {
-  let i = 1;
+  let i: number = 1;
   startBtn.style.animationName = "fade-out";
   images.forEach((item) => {
     item.style.animationDelay = `${i}s`;
     item.style.animationName = "fade-in";
+    item.style.cursor = "pointer";
     i++;
   });
 }
 
+function handleCard(item: HTMLImageElement) {
+  if (listen) {
+    item.style.animationDelay = "0s";
+    item.style.animationName = "slide";
+  }
+}
+
 startBtn.addEventListener("click", startApp);
+images.forEach((item) => {
+  item.addEventListener("click", () => handleCard(item));
+});

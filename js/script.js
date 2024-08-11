1959633d8ed71e9acbd73f819e65ca58ba3a5917
Szymon Dembowski <szymondembowski1234@gmail.com>
2024-08-11 20:22:20 +0200

    Deleted unnecessary comments. Changed some names.

--- js/script.js
+++ js/script.js
@@ -6,39 +6,39 @@
  */
 const startBtn = document.querySelector(".start-btn");
 const images = document.querySelectorAll(".img");
-let listen = true;
+let isClicked = true;
 function startApp() {
     let i = 1;
     startBtn.style.animationName = "fade-out";
-    images.forEach((item) => {
-        item.style.animationDelay = `${i}s`;
-        item.style.animationName = `fade-in${i}`;
+    images.forEach((image) => {
+        image.style.animationDelay = `${i}s`;
+        image.style.animationName = `fade-in${i}`;
         i++;
     });
     setTimeout(() => {
-        images.forEach((item) => {
-            item.style.cursor = "pointer";
+        images.forEach((image) => {
+            image.style.cursor = "pointer";
         });
     }, 6000);
     setTimeout(addOnClick, 6100);
 }
-function handleCard(item) {
-    if (listen) {
-        listen = false;
-        images.forEach((item) => {
-            item.style.cursor = "default";
+function handleCard(image) {
+    if (isClicked) {
+        isClicked = false;
+        images.forEach((image) => {
+            image.style.cursor = "default";
         });
-        item.style.animationDelay = "0s";
-        item.style.animationName = "slide-out";
+        image.style.animationDelay = "0s";
+        image.style.animationName = "slide-out";
         setTimeout(() => {
-            item.setAttribute("src", `./img/${Math.floor(Math.random() * 13 + 1)}.png`);
-            item.style.animationName = "slide-in";
+            image.setAttribute("src", `./img/${Math.floor(Math.random() * 13 + 1)}.png`);
+            image.style.animationName = "slide-in";
         }, 500);
     }
 }
 function addOnClick() {
-    images.forEach((item) => {
-        item.addEventListener("click", () => handleCard(item));
+    images.forEach((image) => {
+        image.addEventListener("click", () => handleCard(image));
     });
 }
-startBtn.addEventListener("click", startApp /*runner*/);
+startBtn.addEventListener("click", startApp);
--- ts/script.ts
+++ ts/script.ts
@@ -7,46 +7,52 @@
 const startBtn: HTMLButtonElement = document.querySelector(".start-btn")!;
 const images: NodeListOf<HTMLImageElement> = document.querySelectorAll(".img");
 
-let listen: boolean = true;
+let isClicked: boolean = true;
 
 function startApp(): void {
   let i: number = 1;
   startBtn.style.animationName = "fade-out";
-  images.forEach((item) => {
-    item.style.animationDelay = `${i}s`;
-    item.style.animationName = `fade-in${i}`;
+
+  images.forEach((image) => {
+    image.style.animationDelay = `${i}s`;
+    image.style.animationName = `fade-in${i}`;
     i++;
   });
+
   setTimeout(() => {
-    images.forEach((item) => {
-      item.style.cursor = "pointer";
+    images.forEach((image) => {
+      image.style.cursor = "pointer";
     });
   }, 6000);
+
   setTimeout(addOnClick, 6100);
 }
 
-function handleCard(item: HTMLImageElement): void {
-  if (listen) {
-    listen = false;
-    images.forEach((item) => {
-      item.style.cursor = "default";
+function handleCard(image: HTMLImageElement): void {
+  if (isClicked) {
+    isClicked = false;
+
+    images.forEach((image) => {
+      image.style.cursor = "default";
     });
-    item.style.animationDelay = "0s";
-    item.style.animationName = "slide-out";
+
+    image.style.animationDelay = "0s";
+    image.style.animationName = "slide-out";
+
     setTimeout(() => {
-      item.setAttribute(
+      image.setAttribute(
         "src",
         `./img/${Math.floor(Math.random() * 13 + 1)}.png`
       );
-      item.style.animationName = "slide-in";
+      image.style.animationName = "slide-in";
     }, 500);
   }
 }
 
 function addOnClick(): void {
-  images.forEach((item) => {
-    item.addEventListener("click", () => handleCard(item));
+  images.forEach((image) => {
+    image.addEventListener("click", () => handleCard(image));
   });
 }
 
-startBtn.addEventListener("click", startApp /*runner*/);
+startBtn.addEventListener("click", startApp);

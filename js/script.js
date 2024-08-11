e9a4ed4c6e73a56244fd489780e932e000415910
Szymon Dembowski <szymondembowski1234@gmail.com>
2024-08-11 20:12:13 +0200

    Fixed the 'too early click' problem. Fixed the cursor set to pointer.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 "use strict";
+/*
+ * TODO
+ * Do the animation of card flipping when clicked.
+ * Make the reset button.
+ */
 const startBtn = document.querySelector(".start-btn");
 const images = document.querySelectorAll(".img");
 let listen = true;
@@ -8,13 +13,21 @@ function startApp() {
     images.forEach((item) => {
         item.style.animationDelay = `${i}s`;
         item.style.animationName = `fade-in${i}`;
-        item.style.cursor = "pointer";
         i++;
     });
+    setTimeout(() => {
+        images.forEach((item) => {
+            item.style.cursor = "pointer";
+        });
+    }, 6000);
+    setTimeout(addOnClick, 6100);
 }
 function handleCard(item) {
     if (listen) {
         listen = false;
+        images.forEach((item) => {
+            item.style.cursor = "default";
+        });
         item.style.animationDelay = "0s";
         item.style.animationName = "slide-out";
         setTimeout(() => {
@@ -23,7 +36,9 @@ function handleCard(item) {
         }, 500);
     }
 }
-startBtn.addEventListener("click", startApp);
-images.forEach((item) => {
-    item.addEventListener("click", () => handleCard(item));
-});
+function addOnClick() {
+    images.forEach((item) => {
+        item.addEventListener("click", () => handleCard(item));
+    });
+}
+startBtn.addEventListener("click", startApp /*runner*/);
--- ts/script.ts
+++ ts/script.ts
@@ -1,20 +1,36 @@
+/*
+ * TODO
+ * Do the animation of card flipping when clicked.
+ * Make the reset button.
+ */
+
 const startBtn: HTMLButtonElement = document.querySelector(".start-btn")!;
 const images: NodeListOf<HTMLImageElement> = document.querySelectorAll(".img");
+
 let listen: boolean = true;
-function startApp() {
+
+function startApp(): void {
   let i: number = 1;
   startBtn.style.animationName = "fade-out";
   images.forEach((item) => {
     item.style.animationDelay = `${i}s`;
     item.style.animationName = `fade-in${i}`;
-    item.style.cursor = "pointer";
     i++;
   });
+  setTimeout(() => {
+    images.forEach((item) => {
+      item.style.cursor = "pointer";
+    });
+  }, 6000);
+  setTimeout(addOnClick, 6100);
 }
 
-function handleCard(item: HTMLImageElement) {
+function handleCard(item: HTMLImageElement): void {
   if (listen) {
     listen = false;
+    images.forEach((item) => {
+      item.style.cursor = "default";
+    });
     item.style.animationDelay = "0s";
     item.style.animationName = "slide-out";
     setTimeout(() => {
@@ -27,7 +43,10 @@ function handleCard(item: HTMLImageElement) {
   }
 }
 
-startBtn.addEventListener("click", startApp);
-images.forEach((item) => {
-  item.addEventListener("click", () => handleCard(item));
-});
+function addOnClick(): void {
+  images.forEach((item) => {
+    item.addEventListener("click", () => handleCard(item));
+  });
+}
+
+startBtn.addEventListener("click", startApp /*runner*/);
